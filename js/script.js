97f80abbbc0f00a966afdd76082ac70acfa61309
Jacek Jarczok <kson.eu@gmail.com>
2019-09-17 16:39:13 +0200

    Little tweaks

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ const arrowsUp = document.getElementById('scrollArrowsUp');
 const anchorArticle = document.getElementById('article');
 
 window.addEventListener('scroll', () => {
-  if (document.documentElement.scrollTop > (anchorArticle.offsetTop - 150) || document.body.scrollTop > (anchorArticle.offsetTop - 150)) {
+  if (document.documentElement.scrollTop > (anchorArticle.offsetTop - 180) || document.body.scrollTop > (anchorArticle.offsetTop - 180)) {
     arrowsDown.classList.remove('isVisible');
     arrowsUp.classList.add('isVisible');
   } else {
